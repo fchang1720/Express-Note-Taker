8804a8b6e782bcbcb5e533dd8737ad3e5a57d507
Crucix <fchang1720@gmail.com>
2022-04-20 18:52:11 -0500

    updated routes/indexs.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
+const api = require('./routes/index.js');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api)
 app.use(express.static('public'));
 
 
