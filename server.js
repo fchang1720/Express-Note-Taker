bf4ed1b4e7eb4c7e1ded1ff456ca9ea756b754cb
Crucix <fchang1720@gmail.com>
2022-04-20 17:09:55 -0500

    added more template files/folders

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const express = require('express');
 // const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.listen(PORT, () => console.log(`Example app listening at http://localhost:3000`));
+
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
